3f3783b28a3272f08f48698c0d16e843c4fe80ab
tokuyama shouta <toku1031ts@gmail.com>
2020-12-13 14:52:43 +0900

    食べ物のスコアの配列を取得

--- dist/food-app.js
+++ dist/food-app.js
@@ -13,9 +13,30 @@ class Food {
 class Foods {
     constructor() {
         this.elements = document.querySelectorAll('.food');
+        this._activeElements = [];
+        this._activeElementsScore = [];
         this.elements.forEach(element => {
             new Food(element);
         });
+    }
+    get activeElements() {
+        this._activeElements = [];
+        this.elements.forEach(element => {
+            if (element.classList.contains('food--active')) {
+                this._activeElements.push(element);
+            }
+        });
+        return this._activeElements;
+    }
+    get activeElementsScore() {
+        this._activeElementsScore = [];
+        this.activeElements.forEach(element => {
+            const foodScore = element.querySelector('.food__Score');
+            if (foodScore) {
+                this._activeElementsScore.push(Number(foodScore.textContent));
+            }
+        });
+        return this._activeElementsScore;
     }
 }
 const foods = new Foods();
