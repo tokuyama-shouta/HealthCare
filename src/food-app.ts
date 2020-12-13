66040b5fb99dfa37153f2927e74c48bb2cf06db1
tokuyama shouta <toku1031ts@gmail.com>
2020-12-13 15:04:59 +0900

    取得したトータルスコアを表示する方法

--- src/food-app.ts
+++ src/food-app.ts
@@ -3,6 +3,9 @@ class Score {
     const foods = new Foods();
     return foods.activeElementsScore.reduce((total, score) => total + score, 0)
   }
+  render(){
+    document.querySelector('.score__number')!.textContent = String(this.totalScore);
+  }
 }
 class Food { 
   constructor(public element: HTMLDivElement){
@@ -10,6 +13,8 @@ class Food {
   }
   clickEventHandler() {
     this.element.classList.toggle('food--active');
+    const score = new Score();
+    score.render();
   }
 }
 class Foods { 
